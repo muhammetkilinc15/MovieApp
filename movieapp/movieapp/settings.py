563d377257f4735bcbb923639fb608f7c3e01572
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-15 19:50:58 +0300

    Added 'movies' app to settings.py

--- movieapp/movieapp/settings.py
+++ movieapp/movieapp/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'movieapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ 
+            BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
