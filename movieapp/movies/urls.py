5be817622c3f15f23c3972132223966832f31dd2
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-16 20:48:36 +0300

    Added name field for url

--- movieapp/movies/urls.py
+++ movieapp/movies/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .import  views
 urlpatterns = [
-    path("",views.index),
-    path("movies",views.movies),
-    path("movies/<slug:slug>",views.movie_details)
+    path("",views.index,name="home_page"),
+    path("movies",views.movies,name="movies_page"),
+    path("movies/<slug:slug>",views.movie_details,name="movie_details")
 ]
 
 # movies/filmadı , movies/1
