fc24a14cad0ae4516ba1009837ea2dd5ee511ea3
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-16 21:28:45 +0300

    Updated the movie_details method

--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -7,6 +7,7 @@ data = {
     "movies": [
         {
         "title":"Film adı 1",
+         "coverImage":"cover3.jpg",
         "description":"film aciklama 1",
         "imageUrl":"m1.jpg",
         "slug":"film-adi-1",
@@ -15,6 +16,7 @@ data = {
         },
         {
         "title":"Film adı 2",
+         "coverImage":"cover2.jpg",
         "description":"film aciklama 2",
         "imageUrl":"m2.jpg",
         "slug":"film-adi-2",
@@ -23,6 +25,7 @@ data = {
         },
         {
         "title":"Film adı 3",
+         "coverImage":"cover1.jpg",
         "description":"film aciklama 3",
         "imageUrl":"m3.jpg",
         "slug":"film-adi-3",
@@ -31,6 +34,7 @@ data = {
         },
         {
         "title":"Film adı 4",
+         "coverImage":"cover1.jpg",
         "description":"film aciklama 4",
         "imageUrl":"m4.jpg",
         "slug":"film-adi-4",
@@ -39,16 +43,18 @@ data = {
         },
         {
         "title":"Film adı 5",
+         "coverImage":"cover2.jpg",
         "description":"film aciklama 4",
-        "imageUrl":"m4.jpg",
+        "imageUrl":"m2.jpg",
         "slug":"film-adi-4",
         "language":"english",
         "date":date(2014,5,25)
         },
         {
         "title":"Film adı 6",
+         "coverImage":"cover3.jpg",
         "description":"film aciklama 4",
-        "imageUrl":"m4.jpg",
+        "imageUrl":"m1.jpg",
         "slug":"film-adi-4",
         "language":"english",
         "date":date(2014,5,25)
@@ -71,7 +77,6 @@ data = {
     ]
 }
 
-
 def index(request):
     movies = data["movies"][-4:]
     sliders = data["slider"]
@@ -87,6 +92,8 @@ def movies(request):
     })
 
 def movie_details(request,slug):
+    movies = data["movies"]
+    selectedMovie = next(movie for movie in movies if movie["slug"]==slug) #  bir liste anlama (list comprehension) ifadesi kullanarak, movies listesindeki belirli bir koşulu sağlayan ilk filmi(next) ile  bulur.
     return  render(request,"movies_details.html",{
-        "slug":slug
+        "movie":selectedMovie
     })
