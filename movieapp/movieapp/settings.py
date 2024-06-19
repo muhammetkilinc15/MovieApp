0e68d33c999ccb7a10da500385bfbdedb2ecbfdd
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-19 20:43:50 +0300

    Added Thousand configuration

--- movieapp/movieapp/settings.py
+++ movieapp/movieapp/settings.py
@@ -131,3 +131,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+USE_THOUSAND_SEPARATOR=True # Gelen Sayılar 3 lük basamağa ayrılı
