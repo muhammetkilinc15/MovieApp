f847221b768695d0b12e00a41bc73797a4802bea
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-19 20:45:17 +0300

    Added MovieAdmin, PersonAdmin and register all

--- movieapp/movies/admin.py
+++ movieapp/movies/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from movies.models import Movie,Person,Video,Genre,Contact
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title","isActive","is_hone",)
+    prepopulated_fields= {"slug":("title",)}
+    last_filter = ("genres","language",)
+    search_fields = ("title","genres",)
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("full_name","gender","duty_type",)
+    list_filter = ("gender","duty_type",)
+    search_fields = ("first_name","last_name",)
+
+admin.site.register(Person,PersonAdmin)
+admin.site.register(Movie,MovieAdmin)
+admin.site.register(Genre)
+admin.site.register(Contact)
+admin.site.register(Video)
