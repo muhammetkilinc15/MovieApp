2d4bf91eb28a2b78841135a7632b374b501485e8
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-19 20:48:24 +0300

    Updated views.py to fetch data from the database

--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -1,66 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import date
-
+from movies.models import Movie
+from django.shortcuts import get_object_or_404
 # Create your views here.
 data = {
-    "movies": [
-        {
-        "title":"Film adı 1",
-         "coverImage":"cover3.jpg",
-        "description":"film aciklama 1",
-        "imageUrl":"m1.jpg",
-        "slug":"film-adi-1",
-        "language":"english",
-        "date":date(2021,10,10)
-        },
-        {
-        "title":"Film adı 2",
-         "coverImage":"cover2.jpg",
-        "description":"film aciklama 2",
-        "imageUrl":"m2.jpg",
-        "slug":"film-adi-2",
-        "language":"english",
-        "date":date(2024,3,9)
-        },
-        {
-        "title":"Film adı 3",
-         "coverImage":"cover1.jpg",
-        "description":"film aciklama 3",
-        "imageUrl":"m3.jpg",
-        "slug":"film-adi-3",
-        "language":"english",
-        "date":date(2019,5,25)
-        },
-        {
-        "title":"Film adı 4",
-         "coverImage":"cover1.jpg",
-        "description":"film aciklama 4",
-        "imageUrl":"m4.jpg",
-        "slug":"film-adi-4",
-        "language":"english",
-        "date":date(2014,5,25)
-        },
-        {
-        "title":"Film adı 5",
-         "coverImage":"cover2.jpg",
-        "description":"film aciklama 4",
-        "imageUrl":"m2.jpg",
-        "slug":"film-adi-4",
-        "language":"english",
-        "date":date(2014,5,25)
-        },
-        {
-        "title":"Film adı 6",
-         "coverImage":"cover3.jpg",
-        "description":"film aciklama 4",
-        "imageUrl":"m1.jpg",
-        "slug":"film-adi-4",
-        "language":"english",
-        "date":date(2014,5,25)
-        },
-    
-    ],
     "slider": [
         {
             "imageUrl":"slider1.jpg",
@@ -78,7 +22,7 @@ data = {
 }
 
 def index(request):
-    movies = data["movies"][-4:]
+    movies = Movie.objects.filter(isActive=True,is_hone=True)
     sliders = data["slider"]
     return  render(request,"index.html",{
         "movies": movies,
@@ -86,14 +30,15 @@ def index(request):
     })
 
 def movies(request):
-    movies = data["movies"]
+    movies = Movie.objects.filter(isActive=True)
     return  render(request,"movies.html",{
         "movies":movies
     })
 
 def movie_details(request,slug):
-    movies = data["movies"]
-    selectedMovie = next(movie for movie in movies if movie["slug"]==slug) #  bir liste anlama (list comprehension) ifadesi kullanarak, movies listesindeki belirli bir koşulu sağlayan ilk filmi(next) ile  bulur.
+    movie = Movie.objects.filter(slug=slug)[0]
+    
     return  render(request,"movies_details.html",{
-        "movie":selectedMovie
+        "movie":movie,
+        "genres":movie.genres.all()
     })
